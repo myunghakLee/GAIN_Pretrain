b13bd3231c62e6c3936e923679fc3d756084cdb3
dnap512 <frshield2@kookmin.ac.kr>
2021-06-30 15:23:21 +0900

    Few shot

--- code/runSemEval.sh
+++ code/runSemEval.sh
@@ -1,2 +1,28 @@
+./run_GAIN_BERT-SemEval.sh 0 "_0.01"
+sudo chmod 777 logs/train_SemEVAL_0.01.log
+tail -f -n 2000 logs/train_SemEVAL_0.01.log
+
+
+./run_GAIN_BERT-SemEval.sh 0 "_0.05"
+sudo chmod 777 logs/train_SemEVAL_0.05.log
+tail -f -n 2000 logs/train_SemEVAL_0.05.log
+
 ./run_GAIN_BERT-SemEval.sh 0 "_0.1"
-tail -f -n 2000 logs/train_SemEVAL_0.1.log
+sudo chmod 777 logs/train_SemEVAL_0.05.log
+tail -f -n 2000 logs/train_SemEVAL_0.05.log
+
+./run_GAIN_BERT-SemEval.sh 0 "_0.2"
+sudo chmod 777 logs/train_SemEVAL_0.05.log
+tail -f -n 2000 logs/train_SemEVAL_0.05.log
+
+./run_GAIN_BERT-SemEval.sh 0 "_0.3"
+sudo chmod 777 logs/train_SemEVAL_0.05.log
+tail -f -n 2000 logs/train_SemEVAL_0.05.log
+
+./run_GAIN_BERT-SemEval.sh 0 "_0.4"
+sudo chmod 777 logs/train_SemEVAL_0.05.log
+tail -f -n 2000 logs/train_SemEVAL_0.05.log
+
+./run_GAIN_BERT-SemEval.sh 0 "_0.5"
+sudo chmod 777 logs/train_SemEVAL_0.05.log
+tail -f -n 2000 logs/train_SemEVAL_0.05.log
